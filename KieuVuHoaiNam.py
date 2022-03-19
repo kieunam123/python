bcbde638e9c33da4f5377eed55d67c03d0efd998
kieunam123 <kieunam011@gmail.com>
2022-03-19 22:29:40 +0700

    updated cau 10

--- KieuVuHoaiNam.py
+++ KieuVuHoaiNam.py
@@ -14,14 +14,14 @@ def bt1():
 def bt2():
     while True:
         try:
-            n = input('nhập vào n (0<n<10): ')
-            if 0<int(n)<10:
+            n = int (input('nhập vào n (0<n<10): '))
+            if 0<n<10:
                 break
             else:
                 print('n phải lớn hơn 0 và nhỏ hơn 10, nhập lại: ')
         except ValueError:
             print('sai dữ liệu đầu vào! nhập lại : ')
-    print('%c + %c%c + %c%c%c + %c%c%c%c ='%(n,n,n,n,n,n,n,n,n,n),int(n)+int(n+n)+int(n+n+n)+int(n+n+n+n))
+    print('%c + %c%c + %c%c%c + %c%c%c%c ='%(n,n,n,n,n,n,n,n,n,n),n+(n+n)+(n+n+n)+(n+n+n+n))
 
 # cau 3
 # Kieu Vu Hoai Nam
@@ -29,36 +29,37 @@ def bt2():
 def bt3():
     while True:
         try:
-            a=input('nhập a : ')
-            b=input('nhập b : ')
-            if int(b)!=0: break
+            a=float(input('nhập a : '))
+            b=float(input('nhập b : '))
+            if b!=0: break
             else:
                 print('vui lòng nhập b khác 0')
         except ValueError:
             print('sai dữ liệu đầu vào! Nhập lại : ')
-    print(a+' + '+b+' =',int(a)+int(b))
-    print(a+' - '+b+' =',int(a)-int(b))
-    print(a+' x '+b+' =',int(a)*int(b))
-    print(a+' / '+b+' =',int(a)//int(b))
-    print(a+' % '+b+' =',int(a)%int(b))
-    print(a+' ^ '+b+' =',int(a)**int(b))
+    print(a+' + '+b+' =',a+b)
+    print(a+' - '+b+' =',a-b)
+    print(a+' x '+b+' =',a*b)
+    print(a+' / '+b+' =',a//b)
+    print(a+' % '+b+' =',a%b)
+    print(a+' ^ '+b+' =',a**b)
 
 # cau 4
 # Kieu Vu Hoai Nam
 # B1807650
 from cmath import pi
+import math
 from tkinter import N
 def bt4():
     while True:
         try:
             n = input('nhập bán kính R : ')   
-            if n!=int(n) : break
+            if n!=float(n) : break
             else : print('vui lòng nhập đúng dữ liệu')     
         except ValueError:
             print('sai dữ liệu đầu vào! Nhập lại: ')
 
-    print('chu vi hình tròn là : ',round((2*pi*int(n)),2))
-    print('diện tích hình tròn là : ',round((round((2*pi*int(n)),2)*pi),2))
+    print('chu vi hình tròn là : ',round((2*pi*float(n)),2))
+    print('diện tích hình tròn là : ',round((round((2*pi*float(n)),2)*pi),2))
 
 # cau 5
 # Kieu Vu Hoai Nam
@@ -70,10 +71,67 @@ def giaithua(n):
 def bt5():
     while True:
         try:
-            n = input('nhập số nguyên n : ')
-            if int(n) > 0 : break
+            n = int(input('nhập số nguyên n : '))
+            if n > 0 : break
             else : print('Vui lòng nhập n > 0')
         except ValueError:
             print('sai dữ liệu đầu vào! Nhập lại: ')
-    print('n! = ',giaithua(int(n)))
+    print('n! = ',giaithua(n))
 
+# cau 6
+# Kieu Vu Hoai Nam
+# B1807650
+def bt6():
+    try:
+        delta = 0
+        a=float(input('nhập a : '))
+        b=float(input('nhập b : '))
+        c=float(input('nhập c : '))
+        delta = ((b)**2)-4*(a)*(c)
+        if delta > 0 :
+            print('phương trình có 2 nghiệm')
+            print('x1 = ',(-(b)+math.sqrt(delta))//(2*(a)))
+            print('x2 = ',(-(b)-math.sqrt(delta))//(2*(a)))
+        elif delta == 0 :
+            print('phương trình có nghiệm kép')
+            print('x1 = x2 = ',-((b)//2*(a)))
+        else : print('phương trình vô nghiệm')
+    except ZeroDivisionError as err: 
+        print("Math Error")
+
+# cau 7
+# Kieu Vu Hoai Nam
+# B1807650
+def bt7():
+    for i in range(5000,7000):
+        if i%7==0 and i%5!=0 :
+            print(i)
+        
+# cau 8
+# Kieu Vu Hoai Nam
+# B1807650      
+def decToBin(x):
+        return int(bin(x)[2:])
+def bt8():
+    n=int(input('nhập số cần đổi sang nhị phân : '))
+    print('hệ nhị phân của %d là : '%n,decToBin(n))
+
+# cau 9
+# Kieu Vu Hoai Nam
+# B1807650     
+def ucln(a,b):
+    if (b == 0):
+        return a
+    return ucln(b, a % b)
+def bcnn(a,b):
+     return int((a * b) / ucln(a, b))
+
+def bt9():
+    a=int(input('nhập a : '))
+    b=int(input('nhập b : '))
+    print('ước chung lớn nhất a và b : ',ucln(a,b))
+    print('bội chung nhỏ nhất a và b : ',bcnn(a,b))
+
+# cau 10
+# Kieu Vu Hoai Nam
+# B1807650 
