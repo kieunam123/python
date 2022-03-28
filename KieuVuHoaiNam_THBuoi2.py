951f24dc1763ec638d72701a193a9d67bedb85f8
kieunam123 <kieunam011@gmail.com>
2022-03-28 23:34:37 +0700

    updated cau 3

--- KieuVuHoaiNam_THBuoi2.py
+++ KieuVuHoaiNam_THBuoi2.py
@@ -16,4 +16,25 @@ def cau1():
 # B1807650
 # Cau 2
 def cau2():
+    upper = ""
+    lower = ""
     s = input("nhập vào câu của bạn : ")
+    for i in s:
+        if i.isupper():
+            upper+=i
+        else : lower+=i
+    print("số chữ hoa là : ", len(upper))
+    print("số chữ thường là : ", len(lower))
+
+# Kieu Vu Hoai Nam
+# B1807650
+# Cau 3
+def cau3():
+    char = ""
+    s = input("nhập vào câu của bạn : ")
+    for i in s:
+        if i.isupper():
+            char+=i
+        else: char+=i.upper()
+    print("các chữ cái đã được in hoa : ",char)
+cau3()
